a380e0e1618dd8ed02ca5c7e8d377dbb75544b68
Martin Dymenstein <martin.dymenstein@thomsonreuters.com>
2023-05-01 22:37:49 -0300

    node lock

--- node-performance/routes/transaction.js
+++ node-performance/routes/transaction.js
@@ -3,25 +3,37 @@ var router = express.Router();
 var trans = require('../models/Transaction');
 var acc = require('../models/Account');
 
+const db = require('../models/db');
+
+
 router.post('/', async function (req, res, next) {
     console.log(req.body);
 
     await trans.create(req.body);
 
-    var account = await acc.findOne( {
-        where : {
-            account: req.body.account
-        },
-        lock: true
-    }).then((a) => {
-        console.log("resultado select : ");
-            acc.update({'balance': Number(req.body.tvalue) + Number(a.dataValues.balance)}, {
-                where : {
-                    account : a.dataValues.account
-                }
-            })
-            console.log(a.dataValues.account);
-    });
+    const transaction = await db.transaction();
+
+    try {
+        const account = await acc.findOne({
+            where: { account: req.body.account },
+            lock: true,
+            transaction: transaction
+        });
+
+        console.log("Resultado select: ", account.dataValues.account);
+
+        const updatedAccount = await acc.update(
+            { balance: Number(req.body.tvalue) + Number(account.dataValues.balance) },
+            { where: { account: account.dataValues.account }, transaction: transaction }
+        );
+
+        console.log("Resultado update: ", updatedAccount);
+
+        await transaction.commit();
+    } catch (error) {
+        await transaction.rollback();
+        console.error(error);
+    }
 
     res.send(req.body);
 
